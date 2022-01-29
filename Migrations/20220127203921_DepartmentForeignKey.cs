c246b347d35305e86e79ecab01064a9affa32bc8
Emanoel Santana <76492419+EmanoelSantana@users.noreply.github.com>
2022-01-28 23:09:12 -0300

    Add files via upload

---
+++ Migrations/20220127203921_DepartmentForeignKey.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers");
+
+            migrationBuilder.RenameColumn(
+                name: "baseSalary",
+                table: "Sellers",
+                newName: "BaseSalary");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Sellers",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers");
+
+            migrationBuilder.RenameColumn(
+                name: "BaseSalary",
+                table: "Sellers",
+                newName: "baseSalary");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Sellers",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_DepartmentId",
+                table: "Sellers",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Services/SellerService.cs
@@ -0,0 +1,70 @@
+﻿using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Seller>> FinddAllAsync()
+        {
+            return await _context.Sellers.ToListAsync(); 
+        }
+
+        public async Task InsertAsync(Seller obj)
+        {
+            _context.Add(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Sellers.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var obj = _context.Sellers.Find(id);
+                _context.Sellers.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException error)
+            {
+                throw new IntegrityException(error.Message);
+            }
+            
+        }
+
+        public async Task UpdateAsync(Seller obj)
+        {
+            bool hasAny = await _context.Sellers.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny) 
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException error) 
+            {
+                throw new DbConcurrencyException(error.Message);
+            }
+        }
+    }
+}
